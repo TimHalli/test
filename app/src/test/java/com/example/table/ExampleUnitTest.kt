f2f894789465fc4800d8dfc5f49c63da5cbf50d1
Tim Halli <t.timhalli@gmail.com>
2023-02-12 19:29:18 +0200

    private fun min()

--- app/src/test/java/com/example/table/ExampleUnitTest.kt
+++ app/src/test/java/com/example/table/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.example.table
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +12,9 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    private fun min(): Boolean {
+        return true
     }
 }
